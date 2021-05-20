f17628ce99295cf63fead83246462997143cca25
zeel sheladiya <46935793+zeelsheladiya@users.noreply.github.com>
2021-05-20 17:48:39 +0530

    Create quote_api.py

---
+++ quote_api.py
@@ -0,0 +1,10 @@
+import os
+import requests
+import json
+import random
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
